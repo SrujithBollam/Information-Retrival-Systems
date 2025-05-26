69405ae6e7ea668b6b021e0f9d29f02429edcc27
SrujithBollam <srujithbollam.official@gmail.com>
2025-05-26 11:28:39 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Srujith",
+    author_email="srujithbollam.official@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],
+)
